bfeb11f7777c605e569a37bf430d9dc119878018
MislavLesin <pripravnik-mislav.lesin@dump.hr>
2020-11-26 21:01:31 +0100

    Drugi commit domaceg rada, trebalo bi sve radit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
 using System.Linq;
 
 namespace ConsoleApp1
@@ -52,7 +54,27 @@ namespace ConsoleApp1
                         BrisanjePjesmePoIndexu(pjesme);
                         break;
 
+                    case 6:
+                        if (pjesme.Count > 0)
+                        {
+                            BrisanjePjesmePoImenu(pjesme);
+                        }
+                        else
+                            Console.WriteLine("Prazna lista!");
+                       
+                        break;
 
+                    case 7:
+                        BrisanjeCijeleListe(pjesme);
+                        break;
+
+                    case 8:
+                        UredivanjeImenaPjesme(pjesme);
+                        break;
+
+                    case 9:
+                        PremijestanjePjesmePoListi(pjesme);
+                        break;
 
                     default:
                         Console.WriteLine("Greska u unosu!!");
@@ -64,56 +86,190 @@ namespace ConsoleApp1
            
 
         }
+
+        static void PremijestanjePjesmePoListi(IDictionary<int, string> pjesme)
+        {
+            List<string> pjesmeUListi = new List<string>();
+            string odabrana = "";
+            IspisListe(pjesme);
+            Console.WriteLine("Unesi ime pjesme kojoj zelis promijenit poziciju");
+            var unos = Console.ReadLine();
+            var index = -1;
+            var ukupniBrojPjesama = pjesme.Count;
+
+            foreach (var pjesma in pjesme)
+            {
+                if (String.Compare(unos.ToLower(), pjesma.Value.ToLower()) == 0)
+                    index = pjesma.Key;
+            }
+
+            if (index == -1)
+            {
+                Console.WriteLine("Nema pjesme tog imena");
+                Console.WriteLine("Dali zelis ponovit?");
+                Console.WriteLine("yes za ponovit");
+                var ponovit = Console.ReadLine();
+                if (String.Compare(ponovit.ToLower(), "yes") == 0)
+                    PremijestanjePjesmePoListi(pjesme);
+            }
+            else
+            {
+                Console.WriteLine("Pijesma imena " + unos + " se nalazi ne meijstu " + index);
+                Console.Write("Unesi novi index: ");
+                var noviIndex = int.Parse(Console.ReadLine());
+                if(noviIndex > pjesme.Count)
+                {
+                    Console.WriteLine("Neispravan unos!!");
+                }
+                else
+                {
+                    foreach (var pjesma in pjesme)                   //sve pisme u listu, a odabrana u varijablu
+                        if (pjesma.Key == index)
+                            odabrana = pjesma.Value;
+                        else
+                            pjesmeUListi.Add(pjesma.Value);
+                    pjesme.Clear();
+
+                    var j = 0;
+                    for(var i = 0; i < ukupniBrojPjesama; i++)
+                    {
+                        if (i == noviIndex)
+                            pjesme.Add(i, odabrana);
+                        else
+                        {
+                            pjesme.Add(i, pjesmeUListi[j]);
+                            j++;
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+
+        static void UredivanjeImenaPjesme(IDictionary<int, string> pjesme)
+        {
+            Console.WriteLine("Unesi ime pjesme koju zelis promijenit");
+            var unos = Console.ReadLine();
+            var index = -1;
+            foreach(var pjesma in pjesme)
+            {
+                if (String.Compare(unos.ToLower(), pjesma.Value.ToLower()) == 0)
+                    index = pjesma.Key;
+
+            }
+
+            if (index == -1)
+            {
+                Console.WriteLine("Nema pjesme tog imena");
+                Console.WriteLine("Dali zelis ponovit?");
+                Console.WriteLine("yes za ponovit");
+                var ponovit = Console.ReadLine();
+                if (String.Compare(ponovit.ToLower(), "yes") == 0)
+                    UredivanjeImenaPjesme(pjesme);
+            }
+            else
+            {
+                Console.WriteLine("Pijesma postoji i ima index " + index);
+                Console.WriteLine("\n");
+                Console.Write("Unesi novo ime pjesme: ");
+                var novoIme = Console.ReadLine();
+                pjesme[index] = novoIme;    
+            }
+               
+
+
+        }
+
+        static void BrisanjeCijeleListe(IDictionary<int, string> pjesme)
+        {
+            Console.WriteLine("Jeste li sigurni da zelite obrisat cijelu listu!?");
+            Console.WriteLine("yes za potvrdit");
+            var unos = Console.ReadLine();
+            if (String.Compare(unos, "yes") == 0)
+            {
+                pjesme.Clear();
+            }
+            else
+                Console.WriteLine("Povratka na meni");
+        }
+
+        static void BrisanjePjesmePoImenu(IDictionary<int, string> pjesme)
+        {
+            List<string> obrisane = new List<string>();
+            var i = 0;
+            var cnt = pjesme.Count;
+            IspisListe(pjesme);
+            Console.WriteLine("Unesi ime pjesme za obrisat: ");
+            var imePjesme = Console.ReadLine();
+            var unos = -1;
+            foreach (var pjesma in pjesme)
+            {
+                if (String.Compare(imePjesme.ToLower(), pjesma.Value.ToLower()) == 0)
+                {
+                    unos = pjesma.Key;
+                    break;
+                }
+            }
+            if(unos != -1)
+            {
+                for (i = unos + 1; i < pjesme.Count; i++)
+                {
+                    obrisane.Add(pjesme[i]);
+                }
+
+                for (i = unos; i < cnt; i++)
+                {
+                    pjesme.Remove(i);
+                }
+                cnt = pjesme.Count;
+                i = 0;
+                for (var j = pjesme.Count; j < (obrisane.Count + cnt); j++)
+                {
+                    pjesme.Add(j, obrisane[i]);
+                    i++;
+                }
+                Console.WriteLine("Uspjesno obrisana pjesma imena " + imePjesme);
+            }
+
+               
+        }
         static void BrisanjePjesmePoIndexu(IDictionary<int, string> pjesme)
         {
             List<string> obrisane = new List<string>();
-            var kolikoIhJeObrisano = 0;
-            var cnt = 0;
+            var i = 0;
+            var cnt = pjesme.Count;
             IspisListe(pjesme);
+            Console.WriteLine("\n\n999 za izlaz");
             Console.WriteLine("\n \nUnesi indeks za brisanje: ");
-            Console.WriteLine("999 za izlaz");
             var unos = int.Parse(Console.ReadLine());
-            if (unos > pjesme.Count)
+
+            if (unos > pjesme.Count && unos != 999)
             {
                 Console.WriteLine("Nesipravan unos!, ponovi!");
                 BrisanjePjesmePoIndexu(pjesme);
             }
             else if (unos == 999)
-                Console.WriteLine("Izlaz"); 
+                Console.WriteLine("Izlaz");
             else
             {
-                for (var i = unos + 1; i < pjesme.Count;i++)          // spremanje u listu
+                for (i = unos + 1; i < pjesme.Count; i++)
                 {
                     obrisane.Add(pjesme[i]);
-                    kolikoIhJeObrisano++;
                 }
 
-                foreach(var pjesma in pjesme)            //brisanje iz dictionarya
+                for (i = unos; i < cnt; i++)
                 {
-                    if(pjesma.Key >= unos)
-                    {
-                        Console.WriteLine("TRenutno brisem "+ pjesma.Value);
-                        pjesme.Remove(pjesma.Key);
-                    }    
+                    pjesme.Remove(i);
                 }
-                Console.WriteLine("U dict ih je sad: " + pjesme.Count);
-                Console.WriteLine("i je sad: " + pjesme.Count);
-                Console.WriteLine("koliko ih je obrisano: " + kolikoIhJeObrisano);
-                //vracanje u dictionary
-                cnt = obrisane.Count;
-
-               /* 
-                pjesme.Add(1, "Druga pisma");
-                pjesme.Add(2, "Treca pisma");
-                pjesme.Add(3, "Cetvrta pisma");
-               */
-                
-                for (var kljuc = (pjesme.Count + kolikoIhJeObrisano); kljuc >= pjesme.Count; kljuc--)       //triba list vrtit od kraja
+                cnt = pjesme.Count;
+                i = 0;
+                for (var j = pjesme.Count; j < (obrisane.Count + cnt); j++)
                 {
-                    pjesme.Add(kljuc - 1, obrisane[cnt - 1]);
-                    cnt--;
+                    pjesme.Add(j, obrisane[i]);
+                    i++;
                 }
-                
             }
         }
 
@@ -164,9 +320,9 @@ namespace ConsoleApp1
         {
             if (pjesme.Count > 0)
             {
-                foreach (var pjesma in pjesme)
+                for(var i = 0; i < pjesme.Count;i++)
                 {
-                    Console.WriteLine(pjesma.Key + " " + pjesma.Value);
+                    Console.WriteLine(i + " " +pjesme[i]);
                 }
             }
             else
